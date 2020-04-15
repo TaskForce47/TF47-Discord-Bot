17368b78fffa73dcdbffe6fe1debe206b3c6e7c9
RednecksRevenge <RednecksRevenge>
2020-04-15 21:22:32 +0200

    refactored output of server command

--- services/Arma3Service.js
+++ services/Arma3Service.js
@@ -1,41 +1,79 @@
-const Gamedig = require('gamedig');
-const Config = require('./../config.json');
+const Gamedig = require("gamedig");
+const Config = require("./../config.json");
 
-module.exports = class Arma3Service{
+module.exports = class Arma3Service {
+  queryServer(msg) {
+    Gamedig.query({
+      type: "arma3",
+      host: Config.Arma3ServerIP,
+      port: Config.Arma3ServerPort
+    }).then(
+      latestServerState => {
+        let mapName = latestServerState.map;
+        mapName = mapName.charAt(0).toUpperCase() + mapName.slice(1);
 
-    queryServer(msg){
-        try{
-            Gamedig.query({
-                type: 'arma3',
-                host: Config.Arma3ServerIP,
-                port: Config.Arma3ServerPort
-            }).then((latestServerState) => {
+        let tmpTime = "";
+        let tmpName = "";
+        latestServerState.players.forEach(player => {
+          tmpTime += `${(
+            Math.round((player.time / 60 / 60) * 100) / 100
+          ).toFixed(2)}h\n`;
+          tmpName += `${player.name}\n`;
+        });
 
-                let mapName = latestServerState.map;
-                mapName = mapName.charAt(0).toUpperCase() + mapName.slice(1);
-                let tmpMessage = '';
+        const embed = {
+          title: "Server Status",
+          fields: [
+            {
+              name: "Server:",
+              value: latestServerState.name
+            },
+            {
+              name: "Current Map",
+              value: mapName,
+              inline: true
+            },
+              { name: "\u200B", value: "\u200B", inline: true },
+            {
+              name: "Current Mission",
+              value: latestServerState.raw.game,
+              inline: true
+            },
+            {
+              name: "IP",
+              value: latestServerState.connect,
+              inline: true
+            },
+            { name: "\u200B", value: "\u200B", inline: true },
+            {
+              name: "Ping",
+              value: latestServerState.ping,
+              inline: true
+            },
+            {
+              name: "Players",
+              value: `${latestServerState.players.length}/${latestServerState.maxplayers}`
+            },
+            {
+              name: "Time",
+              value: tmpTime,
+              inline: true
+            },
+            {
+              name: "Name",
+              value: tmpName,
+              inline: true
+            }
+          ],
+          timestamp: new Date()
+        };
 
-                tmpMessage += `**Server:** ${latestServerState.name}\n`;
-                tmpMessage += `**Map:** ${mapName} | **Mission:** ${latestServerState.raw.game} | **Players:** ${latestServerState.players.length}/${latestServerState.maxplayers}\n`;
-                tmpMessage += `**IP:** ${latestServerState.connect} | **Ping:** ${latestServerState.ping}\n`;
-                tmpMessage += '``` \n';
-                tmpMessage += 'Time | Name \n';
-                tmpMessage += '-----|------\n';
-
-                latestServerState.players.forEach(player => {
-                    tmpMessage += `${(Math.round((player.time/60/60)*100)/100).toFixed(2)}h| ${player.name} \n`;
-                });
-                tmpMessage += ' ```';
-
-                msg.channel.send(tmpMessage);
-            });
-        }
-        catch(e){
-            console.log('Requesting Server Info failed. Error: '+ e);
-            msg.channel.send("Requesting Server Info failed. Try again later");
-        }
-
-    };
-};
-
-
+        msg.channel.send({ embed: embed });
+      },
+      errors => {
+        console.log("Requesting Server Info failed." + errors);
+        msg.channel.send("Requesting Server Info failed. Try again later");
+      }
+    );
+  }
+ };
